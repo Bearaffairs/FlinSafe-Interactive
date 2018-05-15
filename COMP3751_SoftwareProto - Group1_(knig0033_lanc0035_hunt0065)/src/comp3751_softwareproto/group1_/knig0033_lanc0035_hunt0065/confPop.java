1da76fda6ef26162e0ef4ae29bfb404469455ee2
Bearaffairs <itsabearaffair@gmail.com>
2018-05-15 19:28:19 +0930

    fixing close thing for popups

--- COMP3751_SoftwareProto - Group1_(knig0033_lanc0035_hunt0065)/src/comp3751_softwareproto/group1_/knig0033_lanc0035_hunt0065/confPop.java
+++ COMP3751_SoftwareProto - Group1_(knig0033_lanc0035_hunt0065)/src/comp3751_softwareproto/group1_/knig0033_lanc0035_hunt0065/confPop.java
@@ -31,7 +31,7 @@ public class confPop extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jButton1.setText("Ok");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
